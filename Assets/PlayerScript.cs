338b499133ef37ac3430d0ffd2037106f58afe34
Firezaga <106500958+Firezaga@users.noreply.github.com>
2022-06-27 15:47:59 +1000

    Week 2 - Programming
    Completed code for week 2 sprint

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    [SerializeField]
+    public float jumpForce;
+
+    [SerializeField]
+    public int health;
+
+    bool isGrounded = false;
+    Rigidbody2D RB;
+
+    private void Awake()
+    {
+        RB = GetComponent<Rigidbody2D>();
+        health = 100;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isGrounded == true)
+            {
+                RB.AddForce(Vector2.up * jumpForce);
+                isGrounded = false;
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            if (isGrounded == false)
+                isGrounded = true;
+        }
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            Time.timeScale = 0;
+        }
+    }
+}
